78017e0016e403a5bc46831841223dd96d276e0a
Mirek Pesta <mirekpesta@gmail.com>
2025-06-20 11:49:08 +0200

    Prototype01-Working

--- wlan-control/boot.py
+++ wlan-control/boot.py
@@ -1,21 +1,9 @@
 # boot.py -- run on boot-up
 import network, utime
 
-# Replace the following with your WIFI Credentials
-SSID = "kissmyass2"
-SSI_PASSWORD = "jurarose"
 
-def do_connect():
-    import network
-    sta_if = network.WLAN(network.STA_IF)
-    if not sta_if.isconnected():
-        print('connecting to network...')
-        sta_if.active(True)
-        sta_if.connect(SSID, SSI_PASSWORD)
-        while not sta_if.isconnected():
-            pass
-    print('Connected! Network config:', sta_if.ifconfig())
-    
-#print("Connecting to your wifi...")
-#do_connect()
+import relay_webserver
+
+relay_webserver.main()
+
 
--- wlan-control/users_machine.py
+++ wlan-control/users_machine.py
@@ -2,21 +2,17 @@
 
 from machine import Pin, PWM
 
-motorL = PWM(Pin(22,Pin.OUT), freq = 5000, duty = 1023)
-motorR = PWM(Pin(21,Pin.OUT), freq = 5000, duty = 1023)
+motorL = PWM(Pin(22,Pin.OUT), freq = 5000, duty = 0)
+motorR = PWM(Pin(23,Pin.OUT), freq = 5000, duty = 0)
 
-class my_machine:
-  
+class my_machine: 
 
 
-    def __init__(self):
-        
-      print("\n__init__()-----my_machine command-----\n")
-     
+    def __init__(self):        
+      print("\n__init__()-----my_machine command-----\n")     
       print("\n__init__()---------------COMPLETED----\n")
-   
-    def do_commands(self,machine_cmd):     
       
+    def do_commands(self,machine_cmd):           
         print("\n do_commands()-----my_machine command-----")
         print(machine_cmd)
         for key, value in machine_cmd.items():
@@ -25,20 +21,30 @@ class my_machine:
 
       #------------GET operational values for motors
         nMotorL= int(machine_cmd.get("MOTORL"))
-        nMotorL_PWM= 100-int(machine_cmd.get("MOTORL_PWM"))
-      
+        nMotorL_PWM= int(machine_cmd.get("MOTORL_PWM")) #LED is bright when PWM = 0
+
+        nMotorR= int(machine_cmd.get("MOTORR"))
+        nMotorR_PWM= int(machine_cmd.get("MOTORR_PWM"))       
         
-      # ----------------------Calculate Duty
-        print("\nCalculate Motor Duty Cycle-----16H10")  
+      #----------------------Calculate Duty
+        print("BEGIN ----------------Calculate Motor Duty Cycle-----16H10")  
+      
         nDutyL=int(1021/100*(nMotorL_PWM)) # max duty is 1023 LED is LOW
         if nMotorL==0:
-          nDutyL=1023
-        print(f"Duty Left (1023=LED_dim 0=LED_bright): {nDutyL} Motor ON/OFF: {nMotorL}")
+          nDutyL=0
+        print("INFO: (1023=LED_dim 0=LED_bright)")
+        print(f"Duty Left PWM: {nDutyL} Motor ON/OFF: {nMotorL}")
         motorL.duty(nDutyL)
 
-        print("\n do_commands()-----COMPLETED-----1609")
-        
+        nDutyR=int(1021/100*(nMotorR_PWM)) # max duty is 1023 LED is LOW
+        if nMotorR==0:
+          nDutyR=0
+        print("INFO: (1023=LED_dim 0=LED_bright)")
+        print(f"Duty Left PWM: {nDutyR} Motor ON/OFF: {nMotorR}")
+        motorR.duty(nDutyR)
 
+        print("END----------------------Calculate Motor Duty Cycle-----")       
+        # ----------------------
 
 def main():
     print("\n main()===TEST_MY_MACHINE=============================================")
